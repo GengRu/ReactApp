c636da1062539a706e54540464dbd515c219397c
airugg <787922138@qq.com>
2019-11-21 10:07:13 +0800

    登录

---
+++ src/mySql/sql.js
@@ -0,0 +1,28 @@
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    host: 'host:152.136.113.170',
+    user: 'h5_1812',
+    password: 'db!H51812@tx',
+    database: 'H5_1812',
+    port: 3306,
+});
+
+module.exports = {
+    sql(json) {
+        pool.getConnection((err, connection) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            var sql = json.sql;
+            connection.query(sql, json.arr, (err, data) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                json.data(data);
+                connection.release();
+            });
+        });
+    }
+}
