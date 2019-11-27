7b87453e063f3fe2db727a5976b25a4c98a111ac
WongY3306 <2642630866@qq.com>
2019-11-27 10:17:57 +0800

    组件

--- hell_Express/routes/home.js
+++ hell_Express/routes/home.js
@@ -5,11 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function(req, res, next) {
   mysql.sql({
-<<<<<<< HEAD:hell_Express/routes/api.js
-    sql: `select * from one_login where id='${json.id}' and pass='${json.password}'`,
-=======
     sql: `select * from two_homeTitleData`,
->>>>>>> e760c26cc4c6df708fd3abef3cbc8a5a4f3561c9:hell_Express/routes/home.js
     data(data) {
       if (data.length) {
         res.send({
